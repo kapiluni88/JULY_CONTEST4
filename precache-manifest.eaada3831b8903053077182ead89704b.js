9a421db294c7ff797caf6755796efd859e7941f3
kapiluni88 <kapil8745uni@gmail.com>
2023-07-22 23:40:28 +0530

    Updates

---
+++ precache-manifest.eaada3831b8903053077182ead89704b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "284bef5cbf95dea9d50e",
+    "url": "/JULY_CONTEST4/static/css/main.9e7a4318.chunk.css"
+  },
+  {
+    "revision": "284bef5cbf95dea9d50e",
+    "url": "/JULY_CONTEST4/static/js/main.683316d4.chunk.js"
+  },
+  {
+    "revision": "b39b9444d1794caea6d1",
+    "url": "/JULY_CONTEST4/static/js/runtime~main.a66c69ca.js"
+  },
+  {
+    "revision": "dbb0ea2ac7534c64d931",
+    "url": "/JULY_CONTEST4/static/js/2.82b6fa2a.chunk.js"
+  },
+  {
+    "revision": "923fc2f71656286570868824d8ca987d",
+    "url": "/JULY_CONTEST4/index.html"
+  }
+];
